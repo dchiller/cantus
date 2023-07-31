d7f91de298d50b1cf17aa50bfea7a09b8051f146
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-07-31 14:09:22 -0400

    Add navigational buttons

--- nginx/public/node/frontend/public/js/app/manuscript-detail/NavMenuView.js
+++ nginx/public/node/frontend/public/js/app/manuscript-detail/NavMenuView.js
@@ -1,7 +1,10 @@
 import Marionette from "marionette";
 import Backbone from "backbone";
+import $ from 'jquery';
 
 import navitemtemplate from "./nav-item.template.html";
+
+var manuscriptChannel = Backbone.Radio.channel('manuscript');
 
 var NavItemModel = Backbone.Model.extend({});
 
@@ -11,43 +14,53 @@ var NavMenuCollection = Backbone.Collection.extend({
 
 var folioNavOptions = [
     {navTitle:"Folio 2r", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_002r.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_002r.tiff", 
     navDescription: "The Annunciation"},
     {navTitle:"Folio 33r", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_033r.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_033r.tiff", 
     navDescription: "The Nativity"},
     {navTitle:"after Folio 45v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_045_2.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_045_2.tiff", 
     navDescription: "The Adoration of the Magi"},
     { navTitle: "after Folio 50v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_050_1.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_050_1.tiff", 
     navDescription: "The Baptism of Christ"},
     { navTitle: "after Folio 117v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_117_2.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_117_2.tiff", 
     navDescription: "The Agony in the Garden of Gethsemane"},
     { navTitle: "after Folio 124v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_126v.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_126v.tiff", 
     navDescription: "The Resurrection"},
     { navTitle: "after Folio 124v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_126v.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_126v.tiff", 
     navDescription: "The Assembly of the Saints"},
     { navTitle: "after Folio 133v",
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_133_2.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-I_133_2.tiff", 
     navDescription: "Holy Kinship"},
     { navTitle: "Folio A24v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-II_24v.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-II_24v.tiff", 
     navDescription: "Saint Maurice"},
     { navTitle: "Folio A30r", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-II_30r.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-II_30r.tiff", 
     navDescription: "Saint Hubert"},
     { navTitle: "Folio A36v", 
-    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-II_36v.tiff/full/150,/0/default.jpg", 
+    imageLink:"http://salzinnes-antiphonal/iiif/2/Salzinnes-II_36v.tiff", 
     navDescription: "Saint Juliana"}
     ]
 
 
 const NavItemView =  Marionette.ItemView.extend({
-        template: navitemtemplate
+        template: navitemtemplate,
+        ui :{
+            highlightCard: '.highlight-card'
+        },
+        events: {
+            'click @ui.highlightCard': '_goToImage'
+        },
+        _goToImage: function(){
+            manuscriptChannel.request('set:imageURI', this.model.get('imageLink'), {replaceState:true});
+            $('#salzinnesNavMenuModal').modal('hide');
+        }
     });
 
 const NavMenuView = Marionette.CollectionView.extend({
