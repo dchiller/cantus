3d45c77b4d53f61672441cc6bfe78c7aae5adb22
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-08-16 12:24:21 -0400

    Fix refresh timeout

--- nginx/public/node/frontend/public/js/app/manuscript-detail/ManuscriptDetailPageView.js
+++ nginx/public/node/frontend/public/js/app/manuscript-detail/ManuscriptDetailPageView.js
@@ -59,10 +59,10 @@ export default Marionette.LayoutView.extend({
     initialize: function ()
     {
         this._viewportContent = null;
-        var timeoutReset = window.setTimeout(function(){location.reload(true)}, 1200000);
+        var timeoutReset = window.setTimeout(function(){location.reload(true)}, 120000);
         document.body.addEventListener('click', function () {
             window.clearTimeout(timeoutReset);
-            timeoutReset = window.setTimeout(function(){location.reload(true)}, 1200000);
+            timeoutReset = window.setTimeout(function(){location.reload(true)}, 120000);
     });
     },
 
